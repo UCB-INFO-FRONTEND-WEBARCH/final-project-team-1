b7f5e25fed0857b5f89d1270ecea33266fc3ab32
janelupicab <90362158+janelupicab@users.noreply.github.com>
2022-11-20 20:29:52 -0800

    jane updates 11_20

--- app/src/Step_1/Step_1.js
+++ app/src/Step_1/Step_1.js
@@ -6,47 +6,76 @@ import { CsvToHtmlTable } from 'react-csv-to-table';
 
 const Step_1 = () => {
 	
-	const { step, setStep, myData, setData } = useContext(Context);
+	const { step, setStep, myData, setData, header, setHeader} = useContext(Context);
+	var idmax = 0; 
+	let colHeaders = []
 
 
-	
+	//display the CSV file upload as a table: 
 	function viewTable() {
 		var checkForUpload = document.getElementById('uploadButton'); //handles null values of getElementByID 
 		if(checkForUpload){
-			// checkForUpload.addEventListener("click", function(){
 				var fileReader = new FileReader();
 				fileReader.addEventListener('load', function() {
-					// document.getElementById('uploadedFile').innerText = this.result;
 					setData(this.result);
+
+					//split first row of data into an array of strings to get headers 
+					// colHeaders = (myData.split(/\r?\n/)[0]).split(',');
+					setHeader((myData.split(/\r?\n/)[0]).split(','));
+					console.log(header)
 				});
 				fileReader.readAsText(document.querySelector('input').files[0]);
-			// }
-			// )
-	}
+		}
 	}
 	
-	
-	
+	//**attempted to make header into objects so that we could track if it was checked or not but I couldnt get this to work....
+	// function loadHeader(){
+	// 	for (let i = 0; i < colHeaders.length; i++) {
+	// 		let temp = {id: idmax, title: colHeaders[i], status: false}
+	// 		setHeader(...header, temp);
+	// 		idmax += 1;
+	// 	}
+	// }
 
   	return (
    		<div className="Step">
-			<div>
-			<h1>Upload your dataset</h1>
+		<div class="row">
+
+		{/* instructions  */}
+		<div class="column">
+			<h1>Step 1: Upload your dataset</h1>
 			<h3>Your dataset must contain</h3>
 			<ul>
 				<li>predicted outcomes your model determined</li>
 				<li>real outcomes observed</li>
 				<li>attributes you want to audit for bias</li>
 			</ul>
-			</div>
-			<div className = "CSVImport">
-			<h1>REACTJS CSV IMPORT EXAMPLE </h1>
-			<input type="file" accept={".csv"}/>
-			<button id="uploadButton" onClick = {(e) => viewTable()}>Try it</button>
-			<p id = 'uploadedFile'></p>
-			<CsvToHtmlTable data={myData} csvDelimiter="," tableClassName="table table-striped table-hover table-bordered table-sm table-dark"/>
+		</div>
+		</div>
 
-			</div>
+		{/* upload file */}
+		<div class="column">
+		<div className = "CSVImport">
+				<h3>choose your file</h3>
+				<input type="file" accept={".csv"}/>
+				<button id="uploadButton" onClick = {(e) => viewTable()}>Submit</button>
+				<p id = 'uploadedFile'></p>
+				<CsvToHtmlTable data={myData} csvDelimiter="," tableClassName="table table-striped table-hover table-bordered table-sm table-dark"/>
+		</div>
+		</div>
+
+		{/*choose attributes */}
+		<div class="column">
+			<h3>choose your attributes</h3>
+			<button onClick = {(e) => viewTable()}>Go</button>
+			{/* <button onClick = {(e) => loadHeader()}>Go again</button> */}
+
+			<ul>
+			{header.map((head) => { return ( 
+				<li className = 'invis'><input type="checkbox"/>{head}</li> )            
+				})}
+			</ul>
+		</div>
     	</div>
   	);
   };
