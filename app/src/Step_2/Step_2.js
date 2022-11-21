aade47213031103ad4a7b380e5fe9184f128190d
janelupicab <90362158+janelupicab@users.noreply.github.com>
2022-11-20 20:30:14 -0800

    jane updates 11_20

--- app/src/Step_2/Step_2.js
+++ app/src/Step_2/Step_2.js
@@ -1,15 +1,55 @@
 import React, { useState, useEffect, useContext, createContext } from "react";
+import { renderMatches } from "react-router-dom";
 import { Context } from "../App";
 
 
 const Step_2 = () => {
 	
-	const { step, setStep } = useContext(Context);
+	const { step, setStep, myData, setData, header, setHeader, radio, setRadio} = useContext(Context);
+
+	let selectedOption = 'option1';
+
+	function setOption2(){
+		setRadio('option2');
+		console.log(selectedOption)
+	}
+
+	function setOption1(){
+		setRadio('option1');
+	}
 
   	return (
    		<div className="Step">
-        	<a>STEP 2</a>
+		<h1>Step 2: Choose reference groups</h1>
+		<h4>Reference groups are used to calculate relative disparities in our Bias Audit. For example, you might select Male as the reference group for Gender. FairML will then use Male as the baseline to calculate any biases for other groups in the attribute Gender (for Female and Other for example).</h4>
+		<form>
+			<div className="radio">
+          <label>
+            <input 
+				type="radio" 
+				value="option1" 
+				checked={radio === "option1"}
+				onClick = {() => setOption1()}
+				/>
+            Majority group (Automatically select the largest group for every attribute)
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input 
+				type="radio" 
+				value="option2"
+				onClick = {() => setOption2()}
+				checked={radio === "option2"}/>
+            Automatically select group with the lowest bias metric for every attribute
+          </label>
+        </div>
+		</form>
     	</div>
+		
+
+
+
   	);
   };
 export default Step_2;
