96abf92577c04590f01ebd85a72c00e7a3093d01
Han Yang <ppii_775@hotmail.com>
2022-11-25 13:19:25 -0800

    Box styling for Step 3

--- app/src/Step_3/Step_3.js
+++ app/src/Step_3/Step_3.js
@@ -3,6 +3,8 @@ import { Context } from "../App";
 import Button from "@cloudscape-design/components/button";
 import Checkbox from "@cloudscape-design/components/checkbox";
 import Input from "@cloudscape-design/components/input";
+import Box from "@cloudscape-design/components/box";
+import SpaceBetween from "@cloudscape-design/components/space-between";
 
 const Step_3 = () => {
 	
@@ -12,54 +14,73 @@ const Step_3 = () => {
 
   	return (
    		<div className="Step">
-		<div>
-			<h1>Enter your Disparity Intolerance (in %)</h1>
-			<h4>If a specific bias metric for a group is within this percentage of the reference group, this audit will pass</h4>
-			<Input
-      			onChange={({ detail }) => setTemp(detail.value)}
-				value={temp}
-				autoFocus
-				inputMode="numeric"
-				type="number"
-			/>
-			<Button variant="primary" onClick = {() => setPercent(temp)}>
-				Enter
-			</Button>		
-			<p>you inputted: {percent}%</p>
-		</div>
-
-		<div>
-			<h1>Select fairness metrics</h1>
-			<h4>Configure the bias and fairness audit by selecting the fairness measures to audit and the fairness threshold to determine when the audit passes or fails. FairML can compute all the following fairness metrics at once – if you are not sure what you are looking for, select all and we will walk you through the results.</h4>
-		</div>
-			<Checkbox
-				onChange={({ detail }) =>
-					setChecked(detail.checked)
-				} checked={checked}
-			>
-					Equal Parity - Ensure all protected groups are have equal representation in the selected set.
-			</Checkbox>
-			<Checkbox
-				onChange={({ detail }) =>
-					setChecked(detail.checked)
-				} checked={checked}
-			>
-					Proportional Parity - Ensure all protected groups are selected proportional to their percentage of the population.
-			</Checkbox>
-			<Checkbox
-				onChange={({ detail }) =>
-					setChecked(detail.checked)
-				} checked={checked}
-			>
-					False Positive Rate Parity - Ensure all protected groups have the same false positive rates as the reference group.
-			</Checkbox>
-			<Checkbox
-				onChange={({ detail }) =>
-					setChecked(detail.checked)
-				} checked={checked}
-			>
-					False Negative Rate Parity - Ensure all protected groups have the same false negative rates (as the reference group).
-			</Checkbox>
+			<div>
+				<Box margin="l" padding="l">
+					<SpaceBetween direction="vertical" size="xs">
+						<Box variant="h1">Enter your Disparity Intolerance (in %)</Box>
+						<Box variant="h4">If a specific bias metric for a group is within this percentage of the reference group, this audit will pass.</Box>
+					</SpaceBetween>
+				</Box>
+				<Box margin="l" padding="l">
+					<SpaceBetween direction="vertical" size="xs">
+						<Box float="left">
+							<SpaceBetween direction="horizontal" size="xs">
+								<Input
+									onChange={({ detail }) => setTemp(detail.value)}
+									value={temp}
+									autoFocus
+									inputMode="numeric"
+									type="number"
+								/>
+								<Button variant="primary" onClick = {() => setPercent(temp)}>
+									Enter
+								</Button>
+							</SpaceBetween>
+						</Box>
+						<Box variant="p">you inputted: {percent}%</Box>
+					</SpaceBetween>
+				</Box>
+			</div>
+			<div>
+				<Box margin ="l" padding="l">
+					<SpaceBetween direction="vertical" size="xs">
+						<Box variant="h1">Select fairness metrics</Box>
+						<Box variant="h4">Configure the bias and fairness audit by selecting the fairness measures to audit and the fairness threshold to determine when the audit passes or fails. FairML can compute all the following fairness metrics at once – if you are not sure what you are looking for, select all and we will walk you through the results.</Box>
+					</SpaceBetween>
+				</Box>
+			</div>
+			<div>
+				<Box margin="l" padding="l">
+					<Checkbox
+						onChange={({ detail }) =>
+							setChecked(detail.checked)
+						} checked={checked}
+					>
+							Equal Parity - Ensure all protected groups are have equal representation in the selected set.
+					</Checkbox>
+					<Checkbox
+						onChange={({ detail }) =>
+							setChecked(detail.checked)
+						} checked={checked}
+					>
+							Proportional Parity - Ensure all protected groups are selected proportional to their percentage of the population.
+					</Checkbox>
+					<Checkbox
+						onChange={({ detail }) =>
+							setChecked(detail.checked)
+						} checked={checked}
+					>
+							False Positive Rate Parity - Ensure all protected groups have the same false positive rates as the reference group.
+					</Checkbox>
+					<Checkbox
+						onChange={({ detail }) =>
+							setChecked(detail.checked)
+						} checked={checked}
+					>
+							False Negative Rate Parity - Ensure all protected groups have the same false negative rates (as the reference group).
+					</Checkbox>
+				</Box>
+			</div>
     	</div>
   	);
   };
