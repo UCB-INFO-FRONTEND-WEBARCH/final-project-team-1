7dc37f9c209264df644980198be0940cce26b70e
Benjamin Fell <benjafellc@gmail.com>
2022-11-30 17:30:56 -0800

    get started button

--- app/src/Home/components/Hero.js
+++ app/src/Home/components/Hero.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect, useContext} from 'react';
+import { Context } from "../../App";
 import Button from "@cloudscape-design/components/button";
 import Grid from "@cloudscape-design/components/grid";
 
 function Hero() {
+	
+	const { step, setStep } = useContext(Context);
+	
     return (
         <div className="hero">
             <Grid
@@ -12,7 +16,7 @@ function Hero() {
                 <div>
                     <h1>FairML</h1>
                     <p>Audit your machine-learning models for discrimination and bias to make informed decisions around developing and deploying predictive risk-assessment tools.</p>
-                    <Button variant="primary">Get started</Button>
+                    <Button variant="primary" onClick={() => {setStep(1) }}>Get started</Button>
                 </div>
             </Grid>
         </div>
